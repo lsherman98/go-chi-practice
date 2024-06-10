5be1d0b27402e1ea6fab51cfe276acdbf38a4125
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-06-10 19:19:04 -0400

    added routing and endpoints

--- main.go
+++ main.go
@@ -2,29 +2,17 @@ package main
 
 import (
     "fmt"
-    "net/http"
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+    "context"
+    "github.com/lsherman98/go-chi-practice/application"
 )
 
 func main(){
-    router := chi.NewRouter()
-    router.Use(middleware.Logger)
+    app := application.New()
 
-    router.Get("/hello", basicHandler)
-
-    server := &http.Server{
-        Addr: ":3000",
-        Handler: router,
-    }
-
-    err := server.ListenAndServe()
+    err := app.Start(context.TODO())
     if err != nil {
-        fmt.Println("Fialed to listen to server", err)
+        fmt.Println("failed to start app:", err)
     }
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, world!"))
-}
 
